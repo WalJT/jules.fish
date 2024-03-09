7559a28cffbee52ff631a9416f0c78a4ef9a95e6
WalJT <noreply@juliantwalsh.ie>
2024-03-09 10:16:14 +0000

    Set colours

--- jules.fish
+++ jules.fish
@@ -2,9 +2,9 @@ function fish_prompt
   set -l prompt_symbol '> '
   fish_is_root_user; and set prompt_symbol '!> '
 
-  echo $USER@$hostname $prompt_symbol
+  string join '' -- (set_color brgreen) $USER (set_color normal) '@' (set_color bryellow) $hostname  (set_color normal) $prompt_symbol
 end
 
 function fish_right_prompt
-  prompt_pwd --full-length-dirs 1
+  string join '' --  (set_color brblue) $(prompt_pwd --full-length-dirs 1)
 end
