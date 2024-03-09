cf1067f486d1f4b6cd876c01e3b0dba4ac385c35
WalJT <noreply@juliantwalsh.ie>
2024-03-09 08:26:29 +0000

    Use variables for the prompt symbol

--- jules.fish
+++ jules.fish
@@ -1,5 +1,8 @@
 function fish_prompt
-  echo $USER@$hostname '> '
+  set -l prompt_symbol '> '
+  fish_is_root_user; and set prompt_symbol '!> '
+
+  echo $USER@$hostname $prompt_symbol
 end
 
 function fish_right_prompt
